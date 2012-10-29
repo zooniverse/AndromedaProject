0475e674d241536aaed7c2a18b4a075d737f2223
Amit Kapadia <akapadia@cfa.harvard.edu>
2012-10-29 11:53:55 -0500

    Scripts for metadata of subjects

--- utilities/metadata.py
+++ utilities/metadata.py
@@ -3,6 +3,46 @@ import sys
 import numpy
 import pyfits
 
+def header2json():
+    """
+    Convert FITS headers with WCS to JSON.
+    """
+    wcs_keywords = ['WCSAXES', 'NAXIS1', 'NAXIS2', 'CRPIX1', 'CRPIX2', 'CRVAL1', 'CRVAL2', 'CTYPE1', 'CTYPE2', 'CD1_1', 'CD1_2', 'CD2_1', 'CD2_2']
+    
+    data_dir = os.path.join('..', 'data', 'imghead_final')
+    headers = os.listdir(data_dir)
+    
+    for f in headers:
+        path = os.path.join(data_dir, f)
+        header = pyfits.getheader(path)
+        
+        wcs = {}
+        for key in wcs_keywords:
+            wcs[key] = header[key]
+        
+        output = open(os.path.join(data_dir, '..', 'headers', f + ".json"), 'w')
+        output.write(json.dumps(wcs))
+        output.close()
+
+def subimage_centers():
+    
+    def rescale(x, y):
+        x = 215. * x / 650.
+        y = 248. * y / 750.
+        return [x, y]
+    
+    data_dir = os.path.join('..', 'data')
+    data = pyfits.getdata(os.path.join(data_dir, 'phat_subimg-cntrs.fits'))
+    
+    d = {}
+    for row in data:
+        img_id = row[3]
+        [x, y] = rescale(row[6], row[7])
+        d[img_id] = {'x': str(x), 'y': str(y), 'ra': row[4], 'dec': row[5]}
+    output = open(os.path.join(data_dir, 'image-centers.json'), 'w')
+    output.write(json.dumps(d))
+    output.close()
+
 def getYear1Catalog():
     """
     Convert FITS table of year 1 PHAT cluster catalog to CSV
@@ -41,7 +81,7 @@ def getSyntheticCatalog():
             reff = row[17]
             pixradius = reff * 13.12
             f.write("%s, %s, %s, %s, %s, %s, %s, %s\n" % (subimg, fcid, x, y, ra, dec, reff, pixradius))
-        
+
 if __name__ == '__main__':
     # getYear1Catalog()
-    getSyntheticCatalog()
+    # getSyntheticCatalog()
