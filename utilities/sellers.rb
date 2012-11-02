56e616e5aef19c24fc58051693bed716e8cebc99
Amit Kapadia <amit@zooniverse.org>
2012-11-02 11:33:10 -0500

    updated build script

--- utilities/sellers.rb
+++ utilities/sellers.rb
@@ -15,27 +15,41 @@ end
 
 centers = JSON.parse(File.read("#{dirname}/../data/image-centers.json"))
 
+zooniverse_ids = JSON.parse(File.read("#{dirname}/../data/andromeda_subjects.json"))
+id_hash = {}
+zooniverse_ids.each do |item|
+  subimage = item['location'].sub('http://www.andromedaproject.org.s3.amazonaws.com/subjects/standard/', '').split('.')[0]
+  id_hash[subimage] = item['zooniverse_id']
+end
+
 CSV::open('andromeda_subjects.csv', 'wb') do |csv|
-  header = ['name', 'location', 'thumbnail_location', 'ra', 'dec']
+  header = ['zooniverse_id', 'name', 'location', 'thumbnail_location', 'ra', 'dec']
   csv << header
   
   subjects.each do |name|
     name = name.gsub(/\s+/, "")
-    # puts name
+    zooniverse_id = id_hash[name]
+    
+    next if zooniverse_id == nil
+    
     center_name = name.sub('_F475W', '').sub('_sc', '')
+    center = centers[center_name]
     
     row = []
+    row << id_hash[name]
     row << name
     row << "http://www.andromedaproject.org.s3.amazonaws.com/subjects/standard/#{name}.jpg"
     row << "http://www.andromedaproject.org.s3.amazonaws.com/subjects/thumbnail/#{name}.jpg"
-    
-    if centers.has_key?(center_name)
-      center = centers[center_name]
-      row << center['ra']
-      row << center['dec']
-    else
-      puts center_name
-    end
+    row << center['ra']
+    row << center['dec']
+        
+    # if centers.has_key?(center_name)
+    #   center = centers[center_name]
+    #   row << center['ra']
+    #   row << center['dec']
+    # else
+    #   puts center_name
+    # end
     
     csv << row
   end
